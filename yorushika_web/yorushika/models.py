03a229c97f3d1cdba9d35f91c09f37512bf6ec4b
wataru a <minato4561@gmail.com>
2022-06-11 18:30:58 +0900

    feature/add_recommend_page

--- yorushika_web/yorushika/models.py
+++ yorushika_web/yorushika/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 
-class Song(models.Model):
+class Recommend(models.Model):
     title = models.CharField(max_length=150)
     text = models.TextField(max_length=150)
     image = models.ImageField(upload_to='images')
     url = models.URLField(max_length=200, blank=True)
     release_date = models.DateField()
     created_at = models.DateTimeField(auto_now=True)
+    album = models.CharField(max_length=50)
 
     def __str__(self):
         return self.title
@@ -17,6 +18,18 @@ class Overview(models.Model):
     title = models.CharField(max_length=150)
     text = models.TextField(max_length=250)
     image = models.ImageField(upload_to='images')
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=150)
+    text = models.TextField(max_length=150)
+    image = models.ImageField(upload_to='images')
+    url = models.URLField(max_length=200, blank=True)
+    release_date = models.DateField()
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
