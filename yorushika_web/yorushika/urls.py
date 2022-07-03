36bbd4dddc1d6b4f432a5380e5ff04f0feb57e74
wataru a <minato4561@gmail.com>
2022-07-03 16:13:40 +0900

    feature/add_album_page

--- yorushika_web/yorushika/urls.py
+++ yorushika_web/yorushika/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('recommend/', views.recommend, name='recommend'),
     path('album/', views.album, name='album'),
     path('sanctuary/', views.sanctuary, name='sanctuary'),
+    path('song/<int:pk>/', views.song, name='song'),
 ]
--- yorushika_web/yorushika/views.py
+++ yorushika_web/yorushika/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Recommend, Overview
+from django.shortcuts import render, get_object_or_404
+from .models import Recommend, Overview, Album, Song, Sanctuary
 
 
 def index(request):
@@ -9,7 +9,7 @@ def index(request):
 
 
 def overview(request):
-    overviews = Overview.objects.all().order_by('created_at')
+    overviews = Overview.objects.all().order_by('display_order')
     return render(request, 'yorushika/overview.html', {"overviews": overviews})
 
 
@@ -20,8 +20,21 @@ def recommend(request):
 
 
 def album(request):
-    return render(request, 'yorushika/album.html')
+    # -relese_datteにすると降順になる
+    albums = Album.objects.all().order_by('release_date')
+    return render(request, 'yorushika/album.html', {"albums": albums})
+
+
+def song(request, pk):
+    # -relese_datteにすると降順になる
+    albums = Album.objects.all().order_by('release_date')
+    select_album = get_object_or_404(Album, pk=pk)
+    songs = Song.objects.filter(
+        album=select_album).order_by('song_order')
+    return render(request, 'yorushika/song.html', {"songs": songs, "select_album": select_album, "albums": albums})
 
 
 def sanctuary(request):
-    return render(request, 'yorushika/sanctuary.html')
+    # __modelで親のカラムを使用できる
+    sanctuaries = Sanctuary.objects.all().order_by('song__album__release_date')
+    return render(request, 'yorushika/sanctuary.html', {"sanctuaries": sanctuaries})
